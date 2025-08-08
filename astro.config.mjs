e081aab9c2c5d61841dc78514ea1d6412b20ce25
Kevin Quoc <deftik21123@gmail.com>
2025-08-08 19:22:06 +0700

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://k3vin.io",
+	site: "https://k3vinq.github.io",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
